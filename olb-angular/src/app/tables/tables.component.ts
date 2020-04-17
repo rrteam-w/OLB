2e72dee2bb6f55d186f51fe8ef164550543e9f57
Nagaraju JVL <Nagaraju.JVL.Consultan@sothebys.com>
2020-04-17 16:36:21 +0530

    made changes to transfer funds component

--- olb-angular/src/app/tables/tables.component.ts
+++ olb-angular/src/app/tables/tables.component.ts
@@ -18,25 +18,14 @@ export class TablesComponent implements OnInit {
 
   ngOnInit() {
       this.tableData1 = {
-          headerRow: [ 'ID', 'Name', 'Country', 'City', 'Salary'],
+          headerRow: [ 'ID', 'Name', 'Branch', 'Amout', 'Date'],
           dataRows: [
-              ['1', 'Dakota Rice', 'Niger', 'Oud-Turnhout', '$36,738'],
-              ['2', 'Minerva Hooper', 'Curaçao', 'Sinaai-Waas', '$23,789'],
-              ['3', 'Sage Rodriguez', 'Netherlands', 'Baileux', '$56,142'],
-              ['4', 'Philip Chaney', 'Korea, South', 'Overland Park', '$38,735'],
-              ['5', 'Doris Greene', 'Malawi', 'Feldkirchen in Kärnten', '$63,542'],
-              ['6', 'Mason Porter', 'Chile', 'Gloucester', '$78,615']
-          ]
-      };
-      this.tableData2 = {
-          headerRow: [ 'ID', 'Name',  'Salary', 'Country', 'City' ],
-          dataRows: [
-              ['1', 'Dakota Rice','$36,738', 'Niger', 'Oud-Turnhout' ],
-              ['2', 'Minerva Hooper', '$23,789', 'Curaçao', 'Sinaai-Waas'],
-              ['3', 'Sage Rodriguez', '$56,142', 'Netherlands', 'Baileux' ],
-              ['4', 'Philip Chaney', '$38,735', 'Korea, South', 'Overland Park' ],
-              ['5', 'Doris Greene', '$63,542', 'Malawi', 'Feldkirchen in Kärnten', ],
-              ['6', 'Mason Porter', '$78,615', 'Chile', 'Gloucester' ]
+              ['1', 'Nagaraju JVL', 'HDFC', '$100', '04-04-2020'],
+              ['2', 'Krishna', 'SBI', '$200', '06-04-2020'],
+              ['3', 'Mahir Ali', 'ICICI', '$1000', '07-04-2020'],
+              ['4', 'Rakesh', 'Axis', '$500', '08-04-2020'],
+              ['5', 'Ajit Kumar', 'Yes Bank', '$200', '13-04-2020'],
+              ['6', 'Ramesh Nalluri', 'Yes Bank', '$150.50', '13-04-2020']
           ]
       };
   }
