42ecddb5803bae5d99353b9dbb3db233fdd8acd6
ajit-kumar21 <ajit.kumar21@wipro.com>
2020-04-22 11:51:11 +0530

    Corrected formatting of model class

--- db-services/src/main/java/com/rrteam/olb/model/Recepient.java
+++ db-services/src/main/java/com/rrteam/olb/model/Recepient.java
@@ -2,91 +2,101 @@ package com.rrteam.olb.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "recipient")
+@SequenceGenerator(name="recseq", initialValue=3001, allocationSize=998)
 public class Recepient {
 
-	@Column(name = "custid", nullable = true, length = 40)
-	private Long custid;
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "custId", nullable = false)
+	private Customer customer;
 
 	@Column(name = "beneficiary_account_id", nullable = true, length = 45)
-	private String beneficiary_account_id;
+	private String beneficiaryAccountId;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long beneficiary_Id;
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="recseq")
+	@Column(name = "beneficiary_Id")
+	private int beneficiaryId;
 
 	@Column(name = "beneficiary_name", nullable = true, length = 45)
-	private String beneficiary_name;
+	private String beneficiaryName;
 
 	@Column(name = "beneficiary_phone", nullable = true, length = 45)
-	private String beneficiary_phone;
+	private String beneficiaryPhone;
 
 	@Column(name = "beneficiary_email", nullable = true, length = 45)
-	private String beneficiary_email;
+	private String beneficiaryEmail;
 
 	@Column(name = "beneficiary_type", nullable = true, length = 45)
-	private String beneficiary_type;
+	private String beneficiaryType;
+	
+	
 
-	public Long getCustid() {
-		return custid;
+	public Customer getCustomer() {
+		return customer;
 	}
 
-	public void setCustid(Long custid) {
-		this.custid = custid;
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
 	}
 
-	public String getBeneficiary_account_id() {
-		return beneficiary_account_id;
+	public String getBeneficiaryAccountId() {
+		return beneficiaryAccountId;
 	}
 
-	public void setBeneficiary_account_id(String beneficiary_account_id) {
-		this.beneficiary_account_id = beneficiary_account_id;
+	public void setBeneficiaryAccountId(String beneficiaryAccountId) {
+		this.beneficiaryAccountId = beneficiaryAccountId;
 	}
 
-	public Long getBeneficiary_Id() {
-		return beneficiary_Id;
+	public int getBeneficiaryId() {
+		return beneficiaryId;
 	}
 
-	public void setBeneficiary_Id(Long beneficiary_Id) {
-		this.beneficiary_Id = beneficiary_Id;
+	public void setBeneficiaryId(int beneficiaryId) {
+		this.beneficiaryId = beneficiaryId;
 	}
 
-	public String getBeneficiary_name() {
-		return beneficiary_name;
+	public String getBeneficiaryName() {
+		return beneficiaryName;
 	}
 
-	public void setBeneficiary_name(String beneficiary_name) {
-		this.beneficiary_name = beneficiary_name;
+	public void setBeneficiaryName(String beneficiaryName) {
+		this.beneficiaryName = beneficiaryName;
 	}
 
-	public String getBeneficiary_phone() {
-		return beneficiary_phone;
+	public String getBeneficiaryPhone() {
+		return beneficiaryPhone;
 	}
 
-	public void setBeneficiary_phone(String beneficiary_phone) {
-		this.beneficiary_phone = beneficiary_phone;
+	public void setBeneficiaryPhone(String beneficiaryPhone) {
+		this.beneficiaryPhone = beneficiaryPhone;
 	}
 
-	public String getBeneficiary_email() {
-		return beneficiary_email;
+	public String getBeneficiaryEmail() {
+		return beneficiaryEmail;
 	}
 
-	public void setBeneficiary_email(String beneficiary_email) {
-		this.beneficiary_email = beneficiary_email;
+	public void setBeneficiaryEmail(String beneficiaryEmail) {
+		this.beneficiaryEmail = beneficiaryEmail;
 	}
 
-	public String getBeneficiary_type() {
-		return beneficiary_type;
+	public String getBeneficiaryType() {
+		return beneficiaryType;
 	}
 
-	public void setBeneficiary_type(String beneficiary_type) {
-		this.beneficiary_type = beneficiary_type;
+	public void setBeneficiaryType(String beneficiaryType) {
+		this.beneficiaryType = beneficiaryType;
 	}
-
+	
+	
 }
